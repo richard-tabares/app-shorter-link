9cc1be8b9d419060136ee026bd92287d8b7f0e7c
Richard Tabares <richardtabaresb@gmail.com>
2025-08-10 17:01:47 -0500

    Correcion en variables de entorno

--- src/helpers/postShortLink.ts
+++ src/helpers/postShortLink.ts
@@ -3,7 +3,7 @@ export const postShortLink = async (inputUrl: string) => {
 
   const apiUrl = import.meta.env.VITE_API_URL
   
-  const response = await fetch(`${apiUrl}`, {
+  const response = await fetch(`${apiUrl}/idLink`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
